ffafca3cb98ea7db328e869a53c9bf9cc0e806fe
anishajoy06 <anishajoy1201@gmail.com>
2026-03-06 20:23:15 +0530

    Initial commit

---
+++ Task15/Solution.java
@@ -0,0 +1,30 @@
+package Task15;
+import java.util.Scanner;
+
+public class Solution {
+
+    static void staircase(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // print #
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        staircase(n);
+        sc.close();
+    }
+}
